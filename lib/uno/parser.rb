4ddecbf3e46c38cbdb19b02b2b3e2ffa43477795
Magnus Holm <judofyr@gmail.com>
2013-01-14 19:37:38 +0100

    Implement more of the syntax

--- lib/uno/parser.rb
+++ lib/uno/parser.rb
@@ -22,8 +22,75 @@ module Uno
       send("process_#{node.name}", node.value)
     end
 
+    ## Numbers
     def process_num(node)
       [:integer, node[:value].val.to_i]
+    end
+
+    ## Records
+    def process_recempty(node)
+      [:recempty]
+    end
+
+    def process_recmerge(node)
+      [:recmerge, process(node[:rest].val), process(node[:field].val)]
+    end
+
+    def process_recset(node)
+      [:recset, node[:name].val, process(node[:value].val)]
+    end
+
+    def process_recupdate(node)
+      [:recupdate, node[:name].val, process(node[:value].val)]
+    end
+
+    def process_recsplat(node)
+      [:recsplat, process(node[:value].val)]
+    end
+
+    def process_recmethod(node)
+      [:recmethod, node[:name].val, process(node[:value].val)]
+    end
+
+    ## Assignment
+    def process_assign(node)
+      [:assign, node[:name].val, process(node[:value].val)]
+    end
+
+    ## Variables
+    def process_var(node)
+      [:var, node[:name].val]
+    end
+
+    ## Access/methods
+    def process_access(node)
+      [:access, process(node[:base].val), node[:name].val]
+    end
+
+    def process_method(node)
+      [:method, process(node[:base].val), node[:name].val, []]
+    end
+
+    def process_call(node)
+      if node[:args]
+        args = process(node[:args].val)
+      else
+        args = []
+      end
+
+      expr = process(node[:expr].val)
+
+      if expr[0] == :method
+        expr[3].concat args
+        expr
+      else
+        [:call, expr, args]
+      end
+    end
+
+    def process_arg(node)
+      rest = process(node[:rest].val) if node[:rest]
+      [*rest, process(node[:value].val)]
     end
   end
 end
--- test/test_parser.rb
+++ test/test_parser.rb
@@ -7,6 +7,34 @@ module Uno
       exp = Parser.parse("1")
       assert_equal [:integer, 1], exp
     end
+
+    def test_assign
+      exp = Parser.parse("a = 1")
+      assert_equal [:assign, "a", [:integer, 1]], exp
+    end
+
+    def test_recs
+      exp = Parser.parse("{ a: 1, b: 2 }")
+      assert_equal [:recmerge,
+        [:recset, "a", [:integer, 1]],
+        [:recset, "b", [:integer, 2]]], exp
+    end
+
+    def test_access
+      exp = Parser.parse("rec.a")
+      assert_equal [:access, [:var, "rec"], "a"], exp
+    end
+
+    def test_method_calls
+      exp = Parser.parse("point:x")
+      assert_equal [:method, [:var, "point"], "x", []], exp
+
+      exp = Parser.parse("point:translate(1)")
+      assert_equal [:method, [:var, "point"], "translate", [[:integer, 1]]], exp
+
+      exp = Parser.parse("point:translate(1, 2)")
+      assert_equal [:method, [:var, "point"], "translate", [[:integer, 1], [:integer, 2]]], exp
+    end
   end
 end
 
